1503f48dca15558121a71efcdf41ed9d545502da
David Tavadze <tavadzed@gmail.com>
2023-01-23 16:28:50 +0400

    axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Transactions from "./components/Transactions/Transactions";
-import { useSelector } from "react-redux";
+import axios from "./api/axios";
+import { useEffect } from "react";
 
 function App() {
-  const trxs = useSelector(state => state.transactions.transactions);
-  console.log(trxs);
-  
+  useEffect(() => {
+    async function fetchData() {
+      await axios.post("/accounts/filter", {
+        address: "0xC5594070o0o68d9c6597A0160c72CD6ceC2EDd5a",
+        account_type: "user_current",
+        fuck: "bla"
+      });
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <Routes>
