33de1d2a0e2b4c0dee3e1a2ef0cb456457d35102
chubinashvili <chubinashvili@pop-os.localdomain>
2023-01-31 14:14:47 +0400

    last

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,14 +3,16 @@ import { useDispatch, useSelector } from "react-redux";
 import axios from "../../api/axios";
 
 import { Signin } from "@cubitrix/cubitrix-react-ui-module";
+import { useState } from "react";
 
 const Login = () => {
-  const loading = useSelector((state) => state.user.loading);
+  const [loginError, setLoginError] = useState('');
+  const { loading } = useSelector((state) => state.user);
 
   const dispatch = useDispatch();
 
   const handleSubmit = async (currentUser) => {
-    dispatch({ type: "SET_LOADING" });
+    dispatch({ type: "SET_LOADING", payload: { loading: true } });
 
     try {
       const { data } = await axios.post("/api/auth/login ", currentUser);    
@@ -20,21 +22,20 @@ const Login = () => {
         payload: { userId }
       });
     } catch (err) {
-      dispatch({
-        type: "LOGIN_ERROR",
-        payload: { errorMessage: err.response.data.message }
-      });
+      dispatch({ type: "SET_LOADING", payload: { loading: false } });
+      setLoginError(err.response.data.message);
+      setTimeout(() => setLoginError(''), 1500);
     };
   };
 
   return (
-    <div>
+    <>
       {loading ? (
         <div>loading...</div>
       ): (
-        <Signin handleSubmit={handleSubmit} />
+        <Signin handleSubmit={handleSubmit} loginError={loginError} />
       )}
-    </div>
+    </>
   );
 };
 
--- src/store/userReducer.js
+++ src/store/userReducer.js
@@ -1,22 +1,17 @@
 const INIT_STATE = {
     loading: false,
     userId: null,
-    errorMessage: null
 };
   
 const userReducer = (state = INIT_STATE, { type, payload }) => {
     if (type === 'SET_LOADING') {
-        return { ...state, loading: true, errorMessage: null }
+        return { ...state, userId: null, loading: payload.loading }
     }
 
     if (type === "LOGIN") {
         return { ...state, userId: payload.userId, loading: false };
     }
-
-    if (type === "LOGIN_ERROR") {
-        return { ...state, ...INIT_STATE, errorMessage: payload.errorMessage };
-    }
-
+    
     if (type === "LOGOUT") {
         return { ...INIT_STATE };
     }
