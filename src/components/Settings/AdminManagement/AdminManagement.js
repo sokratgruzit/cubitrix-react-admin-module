c6ecb32a6a3629512348329c4d91880545beb4bf
chubinashvili <chubinashvili@pop-os.localdomain>
2023-02-07 13:18:45 +0400

    added confirmPassword field to addAdmin  popUp

--- src/components/Settings/AdminManagement/AdminManagement.js
+++ src/components/Settings/AdminManagement/AdminManagement.js
@@ -16,6 +16,7 @@ const AdminManagement = () => {
         roles: '',
         email: '',
         password: '',
+        confirmPassword: '',
         id: ''
     });
     const [edit, setEdit] = useState(false);
@@ -34,7 +35,7 @@ const AdminManagement = () => {
     const [pageAll, setPageAll] = useState(1);
     const [message, setMessage] = useState('');
 
-    let dynamicDropDown = (email,password,roles, id) => {
+    let dynamicDropDown = (email,roles, id) => {
         const deleteUser = async (email) => {
             try {
                 await axios.post("/api/data/delete-user", {
@@ -45,10 +46,11 @@ const AdminManagement = () => {
             };
         };
 
-        const editUser = async (email, password, roles, id) => {
+        const editUser = async (email, roles, id) => {
             setPopUpData({
                 email,
-                password,
+                password: '',
+                confirmPassword: '',
                 roles,
                 id
             });
@@ -62,7 +64,7 @@ const AdminManagement = () => {
                 list: [
                     {
                         title: "Edit",
-                        onClick: () => editUser(email, password, roles, id),
+                        onClick: () => editUser(email, roles, id),
                         svg: (
                             <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M10.0495 2.00002L3.2078 9.24168C2.94947 9.51668 2.69947 10.0584 2.64947 10.4334L2.34114 13.1333C2.2328 14.1083 2.9328 14.775 3.89947 14.6084L6.5828 14.15C6.9578 14.0834 7.4828 13.8084 7.74114 13.525L14.5828 6.28335C15.7661 5.03335 16.2995 3.60835 14.4578 1.86668C12.6245 0.141685 11.2328 0.750018 10.0495 2.00002Z" stroke="white" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
@@ -154,7 +156,7 @@ const AdminManagement = () => {
                     </div>
                 </div>
                 <div className="table-more">
-                    <MoreButton dropdownData={dynamicDropDown(item.email, item.password, item.roles, item._id)} />
+                    <MoreButton dropdownData={dynamicDropDown(item.email, item.roles, item._id)} />
                 </div>
                 <div className="icon-place">
                     <svg width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -209,6 +211,7 @@ const AdminManagement = () => {
             roles: '',
             email: '',
             password: '',
+            confirmPassword: '',
             id: ''
         });
     };
@@ -246,6 +249,7 @@ const AdminManagement = () => {
                     handlePopUpClose={handlePopUpClose}
                     popUpData={popUpData}
                     setPopUpData={setPopUpData}
+                    edit={edit}
                 />
             )}
         </>
