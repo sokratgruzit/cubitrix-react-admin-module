6c0622f4ff3cbef359fd81e325de306f582ccda8
whatever <eqiza99@gmail.com>
2023-07-05 14:09:41 +0400

    update levels of inputs

--- src/components/Settings/RefferalManagment/ReferralManagment.js
+++ src/components/Settings/RefferalManagment/ReferralManagment.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import {
     Tabs,
     Switches,
@@ -17,6 +16,7 @@ const ReferralManagment = ({
     const [activeTab, setActiveTab] = useState(0);
     const [button, setButton] = useState(false);
     const [inputLevel, setInputLevel] = useState(1);
+    const [rows, setRows] = useState([]);
 
     let data = [
         {
@@ -52,14 +52,56 @@ const ReferralManagment = ({
         console.log("selecthandler");
         console.log(value);
     };
+
     const activeTabHandler = (index) => {
         setActiveTab(index)
     }
 
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setInputs((prevInputs) => ({ ...prevInputs, [name]: value }));
+    };
+
+    const addRow = () => {
+        setRows((prevRows) => [...prevRows, inputs]);
+    };
+
+    const deleteLastRow = () => {
+        setRows((prevRows) => prevRows.slice(0, -1));
+    };
+
+    const [inputs, setInputs] = useState({
+        input: {
+            type: "default",
+            emptyFieldErr: false,
+            inputType: "text",
+            placeholder: "5000",
+            label: `Level ${rows.length + 1} maximum commission`,
+            subLabel: "",
+            onChange: changeHandler,
+            statusCard: '',
+        },
+        input2: {
+            type: "default",
+            emptyFieldErr: false,
+            inputType: "text",
+            placeholder: "5000",
+            label: `Level ${rows.length + 1} maximum commission`,
+            subLabel: "",
+            onChange: changeHandler,
+            statusCard: '',
+            customStyles: { style: `width: 'calc(50% - 10px)'` }
+        },
+    });
+
+    useEffect(() => {
+        setRows([inputs]);
+    }, []);
+
     return (
         <div className={styles.table}>
-            <div style={{borderBottom: 'none'}} className={styles.block}>
-                <h1 className={styles.title}>Referral Managament</h1>
+            <div style={{ borderBottom: 'none' }} className={styles.block}>
+                <h1 className={styles.title}>Referral Management</h1>
                 <Tabs
                     type={'simple'}
                     tabsData={data}
@@ -89,46 +131,55 @@ const ReferralManagment = ({
                     />
                 </div>
                 <div className={styles.block}>
-                    <div className={styles.inputRow}>
-                        <Input
-                            type={"default"}
-                            // value={value}
-                            icon={false}
-                            emptyFieldErr={false}
-                            inputType={"text"}
-                            placeholder={"5000"}
-                            label={`Level ${inputLevel} maximum comission`}
-                            subLabel={""}
-                            onChange={changeHandler}
-                            statusCard={''}
-                            customStyles={{ width: 'calc(50% - 10px)' }}
+                    {rows && rows.map((item, index) => {
+                        console.log(item, 'item')
+                        console.log(index, 'index')
+                        return (
+                            <div className={styles.row} key={index}>
+                                <Input
+                                    type={item.input.type}
+                                    emptyFieldErr={item.input.emptyFieldErr}
+                                    inputType={item.input.inputType}
+                                    placeholder={item.input.placeholder}
+                                    label={`Level ${index + 1} maximum commission`}
+                                    subLabel={item.input.subLabel}
+                                    onChange={item.input.onChange}
+                                    statusCard={item.input.statusCard}
+                                />
+                                <Input
+                                    type={item.input2.type}
+                                    emptyFieldErr={item.input2.emptyFieldErr}
+                                    inputType={item.input2.inputType}
+                                    placeholder={item.input2.placeholder}
+                                    label={`Level ${index + 1} maximum commission`}
+                                    subLabel={item.input2.subLabel}
+                                    onChange={item.input2.onChange}
+                                    statusCard={item.input2.statusCard}
+                                />
+                            </div>
+                        )
+                    })}
+                    <div className={styles.row}>
+                        <Button
+                            label={"Add Level"}
+                            size={"btn-lg"}
+                            type={"btn-primary"}
+                            // arrow={"arrow-right"}
+                            element={"button"}
+                            onClick={addRow}
                         />
-                        <Input
-                            type={"default"}
-                            // value={value}
-                            icon={false}
-                            emptyFieldErr={false}
-                            inputType={"text"}
-                            placeholder={"11%"}
-                            label={`Level ${inputLevel} max comission percentage`}
-                            subLabel={""}
-                            onChange={changeHandler}
-                            statusCard={''}
-                            customStyles={{ width: 'calc(50% - 10px)' }}
+                        <Button
+                            label={"Delete Last Level"}
+                            size={"btn-lg"}
+                            type={"btn-primary"}
+                            // arrow={"arrow-right"}
+                            element={"button"}
+                            onClick={deleteLastRow}
                         />
                     </div>
-                    <Button
-                        label={"Add Level"}
-                        size={"btn-lg"}
-                        type={"btn-primary"}
-                        arrow={"arrow-right"}
-                        element={"button"}
-                        onClick={() => setButton((prevState) => !prevState)}
-                    />
                 </div>
             </div>
         </div>
-
     );
 };
 
