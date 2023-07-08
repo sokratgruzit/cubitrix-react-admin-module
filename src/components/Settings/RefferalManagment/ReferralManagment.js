245b28c7cfa6f488b55ac8bc64a528d700475f1e
whatever <eqiza99@gmail.com>
2023-07-08 12:46:26 +0400

    update

--- src/components/Settings/RefferalManagment/ReferralManagment.js
+++ src/components/Settings/RefferalManagment/ReferralManagment.js
@@ -59,17 +59,6 @@ const ReferralManagment = ({
         },
     ];
 
-    const changeHandler = (i, e) => {
-        console.log(i.target.value);
-    };
-
-    const stepperHandler = (e) => {
-        // setStep(step + 1);
-        // console.log(e, 'stepper index');
-        // console.log(step, 'step')
-
-    }
-
     const selectHandlerUni = (value) => {
         setUniData((prevUniData) => ({
             ...prevUniData,
@@ -98,21 +87,6 @@ const ReferralManagment = ({
             level: binaryLevel
         }));
     }, [binaryLevel]);
-
-    // const fetchData = async () => {
-    //     try {
-    //         const response = await axios.get('/api/data/get_referral_settings');
-    //         console.log(response.data);
-    //     } catch (error) {
-    //         console.error(error);
-    //     }
-    // };
-
-    // useEffect(() => {
-    //     fetchData();
-    // }, [])
-
-
 
     const uniMaxCommissionChangeHandler = (index, value) => {
         setUniData((prevUniData) => {
@@ -168,7 +142,6 @@ const ReferralManagment = ({
                     tabsData={stepper}
                     activeTab={activeTab}
                     setActiveTab={setActiveTab}
-                    onTabClick={(index) => stepperHandler(index)}
                     customStyles={{ width: '100%' }}
                 />
             </div>
@@ -239,7 +212,6 @@ const ReferralManagment = ({
                             type={"btn-primary"}
                             element={"button"}
                             onClick={saveUniDataHandler}
-                        // customStyles={{ width: '30%' }}
                         />
                     </div>
                 </div>
@@ -311,17 +283,29 @@ const ReferralManagment = ({
                             type={"btn-primary"}
                             element={"button"}
                             onClick={saveBinaryDataHandler}
-                        // customStyles={{ width: '30%' }}
                         />
                     </div>
                 </div>
 
             </div>
+            <div className={`${styles.steps} ${styles.underWorking} ${step === 3 ? styles.actived : ""}`}>
+                <svg width="104" height="75" viewBox="0 0 104 75" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path opacity="0.05" d="M96.6042 56.7063C103.822 49.5708 107.82 22.1331 98.7751 13.4263C85.0262 0.191923 62.2753 8.0439 44.8649 1.26497C27.4545 -5.51395 -2.49661e-06 16.2878 -4.12265e-06 34.8875C-6.00351e-06 56.4021 33.2381 76.1446 52.8247 74.9483C72.4114 73.752 88.2825 64.9331 96.6042 56.7063Z" fill="#796AD4" />
+                    <circle cx="95" cy="6" r="2" fill="#796AD4" fillOpacity="0.6" />
+                    <circle cx="88" cy="67" r="2" fill="#796AD4" fillOpacity="0.4" />
+                    <circle cx="10.5" cy="62.5" r="2.5" fill="#796AD4" fillOpacity="0.4" />
+                    <circle cx="13.5" cy="18.5" r="1.5" fill="#796AD4" fillOpacity="0.8" />
+                    <circle cx="15" cy="57" r="1" fill="#796AD4" fillOpacity="0.6" />
+                    <path d="M66 36.25C66 39.3651 65.0763 42.4102 63.3457 45.0002C61.615 47.5903 59.1552 49.609 56.2773 50.8011C53.3993 51.9932 50.2325 52.3051 47.1773 51.6974C44.1221 51.0897 41.3158 49.5896 39.1131 47.3869C36.9104 45.1843 35.4104 42.3779 34.8026 39.3227C34.1949 36.2675 34.5068 33.1007 35.6989 30.2227C36.891 27.3448 38.9097 24.885 41.4998 23.1544C44.0899 21.4237 47.135 20.5 50.25 20.5" stroke="#796AD5" strokeLinecap="round" strokeLinejoin="round" />
+                    <path d="M64.1268 53.2075C65.0543 56.0075 67.1718 56.2875 68.7993 53.8375C70.2868 51.5975 69.3068 49.76 66.6118 49.76C64.6168 49.7425 63.4968 51.3 64.1268 53.2075V53.2075Z" stroke="#796AD5" strokeLinecap="round" strokeLinejoin="round" />
+                    <path d="M55.5 25.75H66" stroke="#796AD5" strokeLinecap="round" strokeLinejoin="round" />
+                    <path d="M55.5 31H60.75" stroke="#796AD5" strokeLinecap="round" strokeLinejoin="round" />
+                </svg>
+                <span>Under Developent</span>
+
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default ReferralManagment;
-{/* <div className={`${styles.steps} ${styles.underWorking} ${step === 3 ? styles.actived : ""}`}>
-                <svg width="104" height="75" viewBox="0 0 104 75" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path opacity="0.05" d="M96.6042 56.7063C103.822 49.5708 107.82 22.1331 98.7751 13.4263C85.0262 0.191923 62.2753 8.0439 44.8649 1.26497C27.4545 -5.51395 -2.49661e-06 16.2878 -4.12265e-06 34.8875C-6.00351e-06 56.4021 33.2381 76.1446 52.8247 */}
