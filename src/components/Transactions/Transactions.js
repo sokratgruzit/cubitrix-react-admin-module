2546a6c1ebcd776c7fee922a583d0172cc70d1ad
whatever <eqiza99@gmail.com>
2023-07-26 14:10:11 +0400

    update

--- src/components/Transactions/Transactions.js
+++ src/components/Transactions/Transactions.js
@@ -41,12 +41,11 @@ const Transactions = (props) => {
   const [selectedTransaction, setSelectedTransaction] = useState(null);
 
   async function fetchData() {
-    await axios
-      .post("/api/data/filter", {
-        type: "transactions",
-        filter: tableFilterOutcomingData,
-        page: pageNow,
-      })
+    await axios.post("/api/data/filter", {
+      type: "transactions",
+      filter: tableFilterOutcomingData,
+      page: pageNow,
+    })
       .then((res) => {
         setPageAll(res.data.success.pages);
         setTd(res.data.success.data);
@@ -55,9 +54,21 @@ const Transactions = (props) => {
   }
 
   useEffect(() => {
-    fetchData();
+    const isMatchingStructure = (
+      tableFilterOutcomingData?.selects?.tx_status === "all" &&
+      Object.getPrototypeOf(tableFilterOutcomingData) === Object.prototype &&
+      Object.getPrototypeOf(tableFilterOutcomingData.selects) === Object.prototype
+    );
+  
+    if (isMatchingStructure) {
+      // If it matches, make tableFilterOutcomingData an empty object
+      setTableFilterOutcomingData({});
+      return; // Exit early without making the request
+    }
+  
+    fetchData(); // Only call fetchData if the structure doesn't match
   }, [tableFilterOutcomingData, pageNow]);
-
+  
   async function newTx() {
     await axios
       .post("/api/transactions/make_transaction", {
@@ -101,7 +112,7 @@ const Transactions = (props) => {
         _id: id,
         tx_status: status,
       });
-      console.log(response);
+      // console.log(response);
       notify("Transaction Status Changed");
       setIsReady(false);
       fetchData();
@@ -126,7 +137,6 @@ const Transactions = (props) => {
     },
   ];
 
-
   let tableData;
   tableData = td.map((item, index) => {
     let dropdownData = [
@@ -143,160 +153,167 @@ const Transactions = (props) => {
       },
     ];
 
-    console.log(item, 'item')
-
     return (
-      <div
-        key={index}
-        className={`table-parent ${mobileExpand === index ? "active" : ""}`}
-        onClick={() => {
-          mobileExpandFunc(index);
-        }}>
-        <div className="table">
-          <div
-            className={`td ${th[0].mobileWidth ? true : false}`}
-            style={{ width: `${mobile ? th[0].mobileWidth : th[0].width}%` }}>
-            <span>{item.tx_hash}</span>
+      <>
+        {td.length === 0 ? (
+          <div >
+            NO ITEMS TO SHOW
           </div>
+        ) : (
           <div
-            className={`td ${th[1].mobileWidth ? true : false}`}
-            style={{ width: `${mobile ? th[1].mobileWidth : th[1].width}%` }}>
-            <span>{item.from}</span>
-          </div>
-          <div
-            className={`td ${th[2].mobileWidth ? true : false}`}
-            style={{ width: `${mobile ? th[2].mobileWidth : th[2].width}%` }}>
-            <span>{item.to}</span>
-          </div>
-          <div
-            className={`td ${th[3].mobileWidth ? true : false}`}
-            style={{ width: `${mobile ? th[3].mobileWidth : th[3].width}%` }}>
-            <span>{item.amount}</span>
-            <span className={`table-currency`}>
-              {tx_currency}
-            </span>
-          </div>
-          <div
-            className={`td ${th[4].mobileWidth ? true : false}`}
-            style={{ width: `${mobile ? th[4].mobileWidth : th[4].width}%` }}>
-            <span>{moment(item.createdAt).format("LL")}</span>
-          </div>
-          <div
-            onClick={() => statusEditHandler(item)}
-            className={`td ${th[5].mobileWidth ? true : false}`}
-            style={{ width: `${mobile ? th[5].mobileWidth : th[5].width}%` }}>
-            {item.tx_type === "payment" ? (
-              <span
-                className={`alert-status-box 
-                            ${item.tx_status === "canceled" && "alert-status-blue"} 
-                            ${item.tx_status === "pending" && "alert-status-yellow"}
-                            ${item.tx_status === "approved" && "alert-status-green"}`}>
-                {item.tx_status}
-              </span>
-            ) : (
-              <>
-                {item.tx_status === "pending" ? (
-                  <Input
-                    type={"lable-input-select"}
-                    icon={false}
-                    emptyFieldErr={false}
-                    defaultData={statuses}
-                    selectHandler={statusSelectHandler}
-                    value={item.tx_status}
-                    active={true}
-                    color={"#FFA726"}
-                  />
-                ) : (
+            key={index}
+            className={`table-parent ${mobileExpand === index ? "active" : ""}`}
+            onClick={() => {
+              mobileExpandFunc(index);
+            }}>
+            <div className="table">
+              <div
+                className={`td ${th[0].mobileWidth ? true : false}`}
+                style={{ width: `${mobile ? th[0].mobileWidth : th[0].width}%` }}>
+                <span>{item.tx_hash}</span>
+              </div>
+              <div
+                className={`td ${th[1].mobileWidth ? true : false}`}
+                style={{ width: `${mobile ? th[1].mobileWidth : th[1].width}%` }}>
+                <span>{item.from}</span>
+              </div>
+              <div
+                className={`td ${th[2].mobileWidth ? true : false}`}
+                style={{ width: `${mobile ? th[2].mobileWidth : th[2].width}%` }}>
+                <span>{item.to}</span>
+              </div>
+              <div
+                className={`td ${th[3].mobileWidth ? true : false}`}
+                style={{ width: `${mobile ? th[3].mobileWidth : th[3].width}%` }}>
+                <span>{item.amount}</span>
+                <span className={`table-currency`}>
+                  {tx_currency}
+                </span>
+              </div>
+              <div
+                className={`td ${th[4].mobileWidth ? true : false}`}
+                style={{ width: `${mobile ? th[4].mobileWidth : th[4].width}%` }}>
+                <span>{moment(item.createdAt).format("LL")}</span>
+              </div>
+              <div
+                onClick={() => statusEditHandler(item)}
+                className={`td ${th[5].mobileWidth ? true : false}`}
+                style={{ width: `${mobile ? th[5].mobileWidth : th[5].width}%` }}>
+                {item.tx_type === "payment" ? (
                   <span
                     className={`alert-status-box 
-                    ${item.tx_status === "canceled" && "alert-status-blue"} 
-                    ${item.tx_status === "pending" && "alert-status-yellow"}
-                    ${item.tx_status === "approved" && "alert-status-green"}`}>
+                         ${item.tx_status === "canceled" && "alert-status-blue"} 
+                         ${item.tx_status === "pending" && "alert-status-yellow"}
+                         ${item.tx_status === "approved" && "alert-status-green"}`}>
                     {item.tx_status}
                   </span>
+                ) : (
+                  <>
+                    {item.tx_status === "pending" ? (
+                      <Input
+                        type={"lable-input-select"}
+                        icon={false}
+                        emptyFieldErr={false}
+                        defaultData={statuses}
+                        selectHandler={statusSelectHandler}
+                        value={item.tx_status}
+                        active={true}
+                        color={"#FFA726"}
+                      />
+                    ) : (
+                      <span
+                        className={`alert-status-box 
+                 ${item.tx_status === "canceled" && "alert-status-blue"} 
+                 ${item.tx_status === "pending" && "alert-status-yellow"}
+                 ${item.tx_status === "approved" && "alert-status-green"}`}>
+                        {item.tx_status}
+                      </span>
+                    )}
+                  </>
                 )}
-              </>
-            )}
-          </div>
-          <div
-            className={`td ${th[6].mobileWidth ? true : false}`}
-            style={{
-              width: `${mobile ? th[6].mobileWidth : th[6].width}%`,
-              paddingRight: "0px",
-              display: "flex",
-              justifyContent: "space-between",
-            }}>
-            <span
-              className={`alert-status-box 
-                  ${item.tx_type === "deposit" && styles.depostit} 
-                  ${item.tx_type === "transfer" && styles.tranfer}
-                  ${item.tx_type === "withdraw" && styles.withdraw}
-                  ${item.tx_type === "payment" && styles.payment}
-                  ${item.tx_type === "internal_transfer" && styles.internal}
-                  ${item.tx_type === "exchange" && styles.exchange}
-                  ${item.tx_type === "bonus" && styles.bonus}
-                `}>
-              {item.tx_type}
-            </span>
-            <div style={{ display: "flex" }} className="table-more">
-              <MoreButton dropdownData={dropdownData} />
-            </div>
-          </div>
-        </div>
-        <div className="icon-place">
-          <svg
-            width="12"
-            height="7"
-            viewBox="0 0 12 7"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M10.299 1.33325L6.47141 5.16089C6.01937 5.61293 5.27968 5.61293 4.82764 5.16089L1 1.33325"
-              stroke="white"
-              strokeWidth="1.5"
-              strokeMiterlimit="10"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </div>
-        <div className="table-mobile">
-          {/* mobile version is shit  */}
-          <div className="table-mobile-content">
-            <div className="td">
-              <div className="mobile-ttl">{th[4].name}</div>
-              <span className={`table-currency`}>{moment(item.createdAt).format("LL")}</span>
-            </div>
-            <div className="td">
-              <div className="mobile-ttl">{th[5].name}</div>
-              {item.type === "payment" ? (
+              </div>
+              <div
+                className={`td ${th[6].mobileWidth ? true : false}`}
+                style={{
+                  width: `${mobile ? th[6].mobileWidth : th[6].width}%`,
+                  paddingRight: "0px",
+                  display: "flex",
+                  justifyContent: "space-between",
+                }}>
                 <span
                   className={`alert-status-box 
-                 ${item.tx_status === "active" && "alert-status-blue"} 
-                 ${item.tx_status === "active1" && "alert-status-yellow"}
-                 ${item.tx_status === "pending" && "alert-status-green"}`}>
-                  {item.tx_status}
+               ${item.tx_type === "deposit" && styles.depostit} 
+               ${item.tx_type === "transfer" && styles.tranfer}
+               ${item.tx_type === "withdraw" && styles.withdraw}
+               ${item.tx_type === "payment" && styles.payment}
+               ${item.tx_type === "internal_transfer" && styles.internal}
+               ${item.tx_type === "exchange" && styles.exchange}
+               ${item.tx_type === "bonus" && styles.bonus}
+             `}>
+                  {item.tx_type}
                 </span>
-              ) : (
-                <Input
-                  type={"lable-input-select"}
-                  icon={false}
-                  emptyFieldErr={false}
-                  defaultData={statuses}
-                  selectHandler={statusSelectHandler}
-                  value={item.tx_status}
-                  active={true}
-                  color={"#FFA726"}
-                />
-              )}
+                <div style={{ display: "flex" }} className="table-more">
+                  <MoreButton dropdownData={dropdownData} />
+                </div>
+              </div>
             </div>
-            <div className="td">
-              <div className="mobile-ttl">{th[6].name}</div>
-              <span>{item.tx_type}</span>
+            <div className="icon-place">
+              <svg
+                width="12"
+                height="7"
+                viewBox="0 0 12 7"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M10.299 1.33325L6.47141 5.16089C6.01937 5.61293 5.27968 5.61293 4.82764 5.16089L1 1.33325"
+                  stroke="white"
+                  strokeWidth="1.5"
+                  strokeMiterlimit="10"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
+            <div className="table-mobile">
+              {/* mobile version is shit  */}
+              <div className="table-mobile-content">
+                <div className="td">
+                  <div className="mobile-ttl">{th[4].name}</div>
+                  <span className={`table-currency`}>{moment(item.createdAt).format("LL")}</span>
+                </div>
+                <div className="td">
+                  <div className="mobile-ttl">{th[5].name}</div>
+                  {item.type === "payment" ? (
+                    <span
+                      className={`alert-status-box 
+              ${item.tx_status === "active" && "alert-status-blue"} 
+              ${item.tx_status === "active1" && "alert-status-yellow"}
+              ${item.tx_status === "pending" && "alert-status-green"}`}>
+                      {item.tx_status}
+                    </span>
+                  ) : (
+                    <Input
+                      type={"lable-input-select"}
+                      icon={false}
+                      emptyFieldErr={false}
+                      defaultData={statuses}
+                      selectHandler={statusSelectHandler}
+                      value={item.tx_status}
+                      active={true}
+                      color={"#FFA726"}
+                    />
+                  )}
+                </div>
+                <div className="td">
+                  <div className="mobile-ttl">{th[6].name}</div>
+                  <span>{item.tx_type}</span>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        )}
+      </>
+
     );
   });
 
